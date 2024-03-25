67dd0722c579c9bd7d158b519758c14c9fdd0c76
Rennan-sbarros <rennan_sbarros@hotmail.com>
2024-03-24 22:53:27 -0300

    feat: criação do interceptor para proteger as rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/Auth.guard';
 
 const routes: Routes = [
   { path: "", pathMatch: "full", redirectTo: "home" },
   {
     path: "home",
     loadChildren: () => import("./pages/home/home.module").then((m) => m.HomeModule),
+    canActivate: [AuthGuard] // retirar posteriomente. Feito apenas para teste.
   },
   {
     path: "registro-usuario",
